969abe36bf965f7c715f5996e10b125b1df32a46
deedeeh <dinahafez.8@gmail.com>
2018-09-25 17:57:22 +0100

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,116 +1,107 @@
 require 'pry'
 
 class Dog
-    attr_accessor :name, :breed
-    attr_reader :id
+  attr_accessor :name, :breed
+  attr_reader :id
 
-    def initialize(id:nil, name:, breed:)
-        @name = name
-        @breed = breed
-        @id = id
-    end
+  def initialize(id:nil, name:, breed:)
+    @name = name
+    @breed = breed
+    @id = id
+  end
 
-    def self.create_table
-        sql = <<-SQL
-            CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);
-            SQL
+  def self.create_table
+    sql = <<-SQL
+        CREATE TABLE dogs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT)
+        SQL
+    DB[:conn].execute(sql)
+  end
 
-        DB[:conn].execute(sql)
-    end
-
-    def self.drop_table
-        sql = <<-SQL
-            DROP TABLE dogs;
-            SQL
-
-        DB[:conn].execute(sql)
-    end
-
-    def self.new_from_db(row)
-        #binding.pry 
-        
-        id = row[0]
-        name = row[1]
-        breed = row[2]
-
-        new_dog = self.new(id:id, name:name, breed:breed)
-        new_dog
-        
-    end
-
-    def self.create(hash)
-        dog = self.new(hash)
-        dog.save
-        dog
-        
-    end
-
-    def save
-        sql = <<-SQL
-            INSERT INTO dogs (name, breed) VALUES (?, ?);
+  def self.drop_table
+    sql = <<-SQL
+        DROP TABLE dogs;
         SQL
 
-        DB[:conn].execute(sql, self.name, self.breed)
+    DB[:conn].execute(sql)
+  end
 
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    new_dog = self.new(id:id, name:name, breed:breed)
+    new_dog
+  end
 
-        #binding.pry
-        self
-    end
+  def self.create(hash)
+    dog = self.new(hash)
+    dog.save
+    dog
+  end
 
-    def self.find_by_id(id)
-        sql = <<-SQL
-            SELECT * FROM dogs WHERE id = ?;
+  def save
+    sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?);
         SQL
 
-        DB[:conn].execute(sql, id).map do |row|
-        self.new_from_db(row)
-        end.first
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
 
-    end
-
-    def self.find_by_name(name)
-        sql = <<-SQL
-            SELECT * FROM dogs WHERE name = ?;
+  def self.find_by_id(id)
+    sql = <<-SQL
+        SELECT * FROM dogs
+        WHERE id = ?;
         SQL
 
-        DB[:conn].execute(sql, name).map do |row|
-        self.new_from_db(row)
-        end.first
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
-    end
-
-    def self.find_by_name_and_breed(name, breed)
-        sql = <<-SQL
-            SELECT * FROM dogs WHERE name = ? AND breed =?;
+  def self.find_by_name(name)
+    sql = <<-SQL
+        SELECT * FROM dogs
+        WHERE name = ?;
         SQL
 
-        DB[:conn].execute(sql, name, breed).map do |row|
-        self.new_from_db(row)
-        end.first
+    DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+    end.first
+  end
 
-    end
+  def self.find_by_name_and_breed(name, breed)
+    sql = <<-SQL
+        SELECT * FROM dogs
+        WHERE name = ?
+        AND breed = ?;
+        SQL
 
-    def update
-        #binding.pry
-        sql = <<-SQL
-            UPDATE dogs SET name = ?, breed = ? WHERE id = ?;
+    DB[:conn].execute(sql, name, breed).map do |row|
+    self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = <<-SQL
+        UPDATE dogs
+        SET name = ?, breed = ?
+        WHERE id = ?;
         SQL
 
     DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+  def self.find_or_create_by(hash)
+    if !self.find_by_name_and_breed(hash[:name], hash[:breed])
+      self.create(hash)
+    else
+      self.find_by_name_and_breed(hash[:name], hash[:breed])
     end
-
-    def self.find_or_create_by(hash)
-        if !self.find_by_name_and_breed(hash[:name], hash[:breed])
-            
-            self.create(hash)
-        
-        else
-            return self.find_by_name_and_breed(hash[:name], hash[:breed])
-        end
-
-    end
-
+  end
 end
-
-#Pry.start
